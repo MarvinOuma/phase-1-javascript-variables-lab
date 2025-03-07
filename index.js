81fda8af6133e1a7fcb531e4bcba11079d9c2b77
MarvinDanielOduor <marvin.ouma@strathmore.edu>
2025-03-07 10:42:15 +0300

    Completed Arithmetic Lab (CodeGrade)

--- index.js
+++ index.js
@@ -46,3 +46,22 @@ function scuberGreetingForFeet(distance) {
     }
   }
   
+// Define variables
+let num1 = 2;
+let num2 = 31;
+
+// Multiply num1 and num2 to get 62
+let multiply = num1 * num2;
+
+// Generate a random integer greater than 0
+let random = Math.floor(Math.random() * 100) + 1;
+
+// Define variables for modulus operation
+let num3 = 10;
+let num4 = 6;
+
+// Find the remainder when num3 is divided by num4 (should be 4)
+let mod = num3 % num4;
+
+// Find the highest number in a set (should be 20)
+let max = Math.max(1, 5, 10, 15, 20);
