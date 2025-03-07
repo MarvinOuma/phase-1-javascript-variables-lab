edaa878dbbb786be0b8bd393a6e66b26d49cff5f
MarvinDanielOduor <marvin.ouma@strathmore.edu>
2025-03-07 10:36:27 +0300

    Completed Control Flow Lab

--- index.js
+++ index.js
@@ -17,3 +17,32 @@ const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
 
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+// Function 1: scuberGreetingForFeet()
+function scuberGreetingForFeet(distance) {
+    if (distance <= 400) {
+      return "This one is on me!";
+    } else if (distance > 2000 && distance <= 2500) {
+      return "I will gladly take your thirty bucks.";
+    } else if (distance > 2500) {
+      return "No can do.";
+    }
+  }
+  
+  // Function 2: ternaryCheckCity()
+  function ternaryCheckCity(city) {
+    return city === "NYC" ? "Ok, sounds good." : "No go.";
+  }
+  
+  // Function 3: switchOnCharmFromTip()
+  function switchOnCharmFromTip(tip) {
+    switch (tip) {
+      case "generous":
+        return "Thank you so much.";
+      case "not as generous":
+        return "Thank you.";
+      default:
+        return "Bye.";
+    }
+  }
+  
